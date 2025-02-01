457ba09a20bb3d32811accd0c930892b3b575710
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-02-01 20:24:50 +0100

    update

---
+++ src/utils.ts
@@ -0,0 +1,25 @@
+import { CLICMD } from "./command.js";
+
+export class CLIUtils {
+
+    static canRunInCurrentEnvironment(cmd: CLICMD) {
+        if (cmd.environment === "all") return true;
+        if (Main.environment === "command") {
+            return cmd.environment === "shell";
+        }
+        return cmd.environment === "runtime";
+    }
+
+    static parsePArgs(parent_args: string[], appendSpaceIFNotEmpty = false): string {
+        let parent_args_str = parent_args.join(" ");
+        if (appendSpaceIFNotEmpty && parent_args_str) {
+            parent_args_str += " ";
+        }
+        return parent_args_str;
+    }
+
+    static invalidNumberOfArguments(): void {
+        cli.cmd.info("Invalid number of arguments!");
+    }
+
+}
