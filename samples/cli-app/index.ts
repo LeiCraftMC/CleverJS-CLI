eff91a77550687a0e0736b8d9bd466272d55f0b8
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-02-06 19:23:49 +0100

    update

--- samples/cli-app/index.ts
+++ samples/cli-app/index.ts
@@ -1,9 +1,12 @@
 
 import { CLIApp } from '@cleverjs/cli';
+import { VersionCMD } from './commands/version';
 
 class MyCliApp extends CLIApp {
     
-    
+    protected registerCommands(): void {
+        this.register(new VersionCMD());
+    }
     
 }
 
