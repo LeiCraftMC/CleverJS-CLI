4df80cfd679f8c2c11f4165e82c3faf3f3d2e138
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-02-09 11:39:23 +0100

    update

--- samples/cli-app/index.ts
+++ samples/cli-app/index.ts
@@ -7,6 +7,11 @@ class MyCliApp extends CLIApp {
     protected registerCommands(): void {
         this.register(new VersionCMD());
     }
+
+    protected async run_empty(meta: any): Promise<void> {
+        this.run_help(meta);
+    }
     
 }
 
+new MyCliApp("shell").handle(process.argv.slice(2).join(" "));
